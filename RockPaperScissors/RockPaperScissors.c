e085d11afdb73535c972282ab1c92a9d4a8f8dab
DamianWilson2222 <dwilson9978@conestogac.on.ca>
2025-11-08 20:52:26 -0500

    Make function code

--- RockPaperScissors/RockPaperScissors.c
+++ RockPaperScissors/RockPaperScissors.c
@@ -2,7 +2,26 @@
 
 char* RockPaperScissors(char* p1, char* p2) {
 
-	return 0;
+	
+	if (p1 == p2) {
+
+		return "draw";
+	}
+
+	else if (((p1 == "Rock") && (p2 == "Scissors")) || ((p1 == "Scissors") && (p2 == "Paper")) || ((p1 == "Paper") && (p2 == "Rock"))) {
+
+		return "Player1";
+	}
+
+	else if (((p2 == "Rock") && (p1 == "Scissors")) || ((p2 == "Scissors") && (p1 == "Paper")) || ((p2 == "Paper") && (p1 == "Rock"))) {
+
+		return "Player2";
+	}
+
+	else {
+
+		return "Invalid";
+	}
 }
 
 int main() {
