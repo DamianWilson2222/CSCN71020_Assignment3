a726dc859b79aa24c96270078f2c5134adad8356
DamianWilson2222 <dwilson9978@conestogac.on.ca>
2025-11-08 19:38:58 -0500

    Code 11 test methods

--- TestRockPaperScissors/TestRockPaperScissors.cpp
+++ TestRockPaperScissors/TestRockPaperScissors.cpp
@@ -11,46 +11,79 @@ namespace TestRockPaperScissors
 		
 		TEST_METHOD(RockPaperScissors_Player1RockPlayer2Rock_ReturnDraw)
 		{
+			char* player1_input = "Rock";
+			char* player2_input = "Rock";
+			Assert::AreEqual("Draw", RockPaperScissors(player1_input, player2_input), "Function Failed");
 		}
 
-		TEST_METHOD(RockPaperScissors_Player1RockPlayer2Rock_ReturnDraw)
+		TEST_METHOD(RockPaperScissors_Player1RockPlayer2Scissors_ReturnPlayer1)
 		{
+			char* player1_input = "Rock";
+			char* player2_input = "Scissors";
+			Assert::AreEqual("Player1", RockPaperScissors(player1_input, player2_input), "Function Failed");
 		}
 
-		TEST_METHOD(RockPaperScissors_Player1RockPlayer2Rock_ReturnDraw)
+		TEST_METHOD(RockPaperScissors_Player1RockPlayer2Paper_ReturnPlayer2)
 		{
+			char* player1_input = "Rock";
+			char* player2_input = "Paper";
+			Assert::AreEqual("Player2", RockPaperScissors(player1_input, player2_input), "Function Failed");
 		}
 
-		TEST_METHOD(RockPaperScissors_Player1RockPlayer2Rock_ReturnDraw)
+		TEST_METHOD(RockPaperScissors_Player1ScissorsPlayer2Rock_ReturnPlayer2)
 		{
+			char* player1_input = "Scissors";
+			char* player2_input = "Rock";
+			Assert::AreEqual("Player2", RockPaperScissors(player1_input, player2_input), "Function Failed");
 		}
 
-		TEST_METHOD(RockPaperScissors_Player1RockPlayer2Rock_ReturnDraw)
+		TEST_METHOD(RockPaperScissors_Player1ScissorsPlayer2Scissors_ReturnDraw)
 		{
+			char* player1_input = "Scissors";
+			char* player2_input = "Scissors";
+			Assert::AreEqual("Draw", RockPaperScissors(player1_input, player2_input), "Function Failed");
 		}
 
-		TEST_METHOD(RockPaperScissors_Player1RockPlayer2Rock_ReturnDraw)
+		TEST_METHOD(RockPaperScissors_Player1ScissorsPlayer2Paper_ReturnPlayer1)
 		{
+			char* player1_input = "Scissors";
+			char* player2_input = "Paper";
+			Assert::AreEqual("Player1", RockPaperScissors(player1_input, player2_input), "Function Failed");
 		}
 
-		TEST_METHOD(RockPaperScissors_Player1RockPlayer2Rock_ReturnDraw)
+		TEST_METHOD(RockPaperScissors_Player1PaperPlayer2Rock_ReturnPlayer1)
 		{
+			char* player1_input = "Paper";
+			char* player2_input = "Rock";
+			Assert::AreEqual("Player1", RockPaperScissors(player1_input, player2_input), "Function Failed");
 		}
 
-		TEST_METHOD(RockPaperScissors_Player1RockPlayer2Rock_ReturnDraw)
+		TEST_METHOD(RockPaperScissors_Player1PaperPlayer2Scissors_ReturnPlayer2)
 		{
+			char* player1_input = "Paper";
+			char* player2_input = "Scissors";
+			Assert::AreEqual("Player2", RockPaperScissors(player1_input, player2_input), "Function Failed");
 		}
 
-		TEST_METHOD(RockPaperScissors_Player1RockPlayer2Rock_ReturnDraw)
+		TEST_METHOD(RockPaperScissors_Player1PaperPlayer2Paper_ReturnDraw)
 		{
+			char* player1_input = "Paper";
+			char* player2_input = "Paper";
+			Assert::AreEqual("Draw", RockPaperScissors(player1_input, player2_input), "Function Failed");
 		}
 
-		TEST_METHOD(RockPaperScissors_Player1RockPlayer2Rock_ReturnDraw)
+		TEST_METHOD(RockPaperScissors_Player1InvalidPlayer2Rock_ReturnDraw)
 		{
+			char* player1_input = "Banana";
+			char* player2_input = "Rock";
+			Assert::AreEqual("Invalid", RockPaperScissors(player1_input, player2_input), "Function Failed");
 		}
 
-		TEST_METHOD(RockPaperScissors_Player1RockPlayer2Rock_ReturnDraw)
+		TEST_METHOD(RockPaperScissors_Player1RockPlayer2Invlaid_ReturnDraw)
 		{
+			char* player1_input = "Rock";
+			char* player2_input = "Orange";
+			Assert::AreEqual("Invalid", RockPaperScissors(player1_input, player2_input), "Function Failed");
 		}
 	};
 }
